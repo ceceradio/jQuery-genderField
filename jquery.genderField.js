714f123a57cad2a0edf821fe4224f39bd3acfb9f
Charlie <me@satsuki.tv>
2016-03-12 18:16:14 -0500

    fixed some massive scoping problems, modified the default genders, and changed the way that the dropdown is populated (in a more angular-esque fashion, the elements are not removed and added, but rather hidden or shown)

--- jquery.genderField.js
+++ jquery.genderField.js
@@ -4,9 +4,12 @@
         "Androgyne",
         "Androgynous",
         "Bigender",
+        "Boi",
+        "Butch",
         "Cis Female",
         "Cis Feminine",
         "Cis Male",
+        "Cis Man",
         "Cis Masculine",
         "Cis Woman",
         "Demigirl",
@@ -26,16 +29,108 @@
         "Nonbinary",
         "Other",
         "Pangender",
+        "Third Gender",
         "Trans Female",
+        "Trans Feminine",
         "Trans Male",
         "Trans Man",
-        "Trans Feminine",
-        "Trans Femme",
+        "Trans Masculine",
         "Trans Woman",
         "Woman"
     ];
+    // generate search map
+    function generateSearchMap(genders) {
+        var genderSearchMap = {};
+        for(var i=0;i<genders.length;i++) {
+            var components = genders[i].split(/\s+/);
+            for(var j = 0; j<components.length; j++) {
+                var lowercaseTerm = components[j].toLowerCase();
+                for (var subTermLength = 1; subTermLength <= lowercaseTerm.length; subTermLength++) {
+                    var subTerm = lowercaseTerm.substring(0,subTermLength);
+                    if (!genderSearchMap.hasOwnProperty(subTerm)) {
+                        genderSearchMap[subTerm] = [];
+                    }
+                    genderSearchMap[subTerm].push(genders[i]);
+                }
+            }
+        }
+        return genderSearchMap;
+    }
+    // size our dropdown box based on the input box
+    function resize(input) {
+        var dropdown = input.parent().find(".genderField-dropdown");
+        var innerWidth = dropdown.outerWidth() - dropdown.innerWidth();
+        var totalWidth = input.outerWidth()-innerWidth;
+        var totalHeight = input.outerHeight()+input.position().top;
+        var leftOffset = (input.position().left+parseInt(input.css('margin-left'),10));
+        // possible ie7 workaround for position().left returning an incorrect value
+        if (input.position().left==0 && parseInt(input.css('left'),10) > 0) {
+            leftOffset += parseInt(input.css('left'),10)
+        }
+        dropdown.width(totalWidth)
+            .css('top',totalHeight.toString()+"px")
+            .css('left',leftOffset.toString()+"px");
+    }
+    
+    function findGenders(allGenders, genderSearchMap, dropdown, value) {
+        // split our value up by components to search
+        var valueComponents = value.split(/\s+/);
+        var foundGenders = [];
+        if (value) {
+        for (var k = 0; k < valueComponents.length; k++) {
+            var lowercaseTerm = valueComponents[k].toLowerCase();
+            if (genderSearchMap.hasOwnProperty(lowercaseTerm)) {
+            foundGenders = foundGenders.concat(genderSearchMap[lowercaseTerm]);
+            }
+        }
+        }
+        else {
+        foundGenders = allGenders;
+        }
+        foundGenders = uniq(foundGenders);
+        return foundGenders;
+    }
+    // sort an array and ensure uniqueness
+    function uniq(a) {
+        return a.sort().filter(function(item, pos, ary) {
+            return !pos || item != ary[pos - 1];
+        })
+    }
     $.fn.genderField = function(action) {
-        genders.sort();
+        var localGenders = genders.slice();
+        var genderSearchMap = {};
+        localGenders.sort();
+        function addDropdownGenders(dropdown) {
+            dropdown.empty();
+            for (var i = 0; i < localGenders.length; i++) {
+                var genderLabel = (options.lowercase)?localGenders[i].toLowerCase():localGenders[i];
+                var element = $("<span class='"+localGenders[i].replace(/\W/g,"_")+"'>"+genderLabel+"</span>");
+                element.appendTo(dropdown);
+            }
+            if (options.allowPreferNotToDisclose) {
+                $("<span class='prefer_not_to_disclose'>prefer not to disclose</span>").appendTo(dropdown)
+            }
+            setAllClickEvents(dropdown);
+        }
+        function setDropdownGenders(dropdown, genders) {
+            dropdown.find('span').hide().addClass('hiddenGender').removeClass('shownGender');
+            for (var i = 0; i < genders.length; i++) {
+                dropdown.find("."+genders[i].replace(/\W/g,"_")).show().addClass('shownGender').removeClass('hiddenGender');
+            }
+            if (dropdown.find("span").length == 0)
+                dropdown.hide();
+            else
+                dropdown.show();
+        }
+        function setAllClickEvents(dropdown) {
+            dropdown.find('span').click(function() {
+                dropdown.parent().find('input').val($(this).text()).focus();
+                var foundGenders = findGenders(localGenders, genderSearchMap, dropdown, $(this).text());
+                setDropdownGenders(dropdown, foundGenders);
+                if (dropdown.find("span").length == 1)
+                    dropdown.hide();
+            })
+        }
         if (typeof action == "undefined" || typeof action == "object") {
             var options = {
                 showArrow: true,
@@ -47,114 +142,31 @@
                 options = $.extend({}, options, action);
             }
             if (typeof options.additionalGenders.length !== "undefined" && options.additionalGenders.length>0) {
-                genders = genders.concat(options.additionalGenders);
+                localGenders = localGenders.concat(options.additionalGenders);
             }
             if (typeof options.genders !== "undefined" && typeof options.genders.length !== "undefined" && options.genders.length>0) {
-                genders = options.genders;
+                localGenders = options.genders;
             }
-            // size our dropdown box based on the input box
-            function resize(input) {
-                var dropdown = input.parent().find(".genderField-dropdown");
-                var innerWidth = dropdown.outerWidth() - dropdown.innerWidth();
-                var totalWidth = input.outerWidth()-innerWidth;
-                var totalHeight = input.outerHeight()+input.position().top;
-                var leftOffset = (input.position().left+parseInt(input.css('margin-left'),10));
-                // possible ie7 workaround for position().left returning an incorrect value
-                if (input.position().left==0 && parseInt(input.css('left'),10) > 0) {
-                    leftOffset += parseInt(input.css('left'),10)
-                }
-                dropdown.width(totalWidth)
-                    .css('top',totalHeight.toString()+"px")
-                    .css('left',leftOffset.toString()+"px");
-            }
-            // find genders based on the current input
-            // insert into the correct position alphabetically
-            function findGenders(dropdown,value) {
-                var components;
-                var count = 0;
-                // split our value up by components to search
-                var valueComponents = value.split(/\s+/);
-                var found = false;
-                // for each gender
-                for(var i=0;i<genders.length;i++) {
-                    found = false;
-                    // we split it into tokens based on whitespace
-                    components = genders[i].split(/\s+/);
-                    // and search each token of the gender
-                    for(var j = 0; j<components.length; j++) {
-                        // against each token of our value
-                        for (var k = 0; k < valueComponents.length; k++) {
-                            // if there is a partial match starting at the beginning of both tokens
-                            if (components[j].toLowerCase().indexOf(valueComponents[k].toLowerCase()) === 0) {
-                                // insert or append if the element doesn't exist in the list
-                                if (dropdown.find("span."+genders[i].replace(/\W/g,"_")).length==0) {
-                                    var added = false
-                                    var element = $("<span class='"+genders[i].replace(/\W/g,"_")+"'>"+genders[i]+"</span>");
-                                    // search through all genders listed to find where to insert
-                                    dropdown.find("span").each(function() {
-                                        if ($(this).text() > genders[i] && !added) {
-                                            element.insertBefore($(this));
-                                            // record that we added this element so we don't add it again
-                                            added = true;
-                                        }
-                                    });
-                                    // if we didnt add our element by the end, do it now
-                                    if (!added)
-                                        element.appendTo(dropdown);
-                                    // add our click event to the element
-                                    element.click(function() {
-                                        dropdown.parent().find('input').val($(this).text()).focus();
-                                        findGenders(dropdown, $(this).text());
-                                        if (dropdown.find("span").length == 1)
-                                            dropdown.hide();
-                                    });
-                                }
-                                // we found a match for this gender, so record it and break out
-                                found = true;
-                                break;
-                            }
-                        }
-                        // break out if we found a match
-                        if (found) break;
-                    }
-                    // if there was no match found, remove the gender we're looking at from the list
-                    if (!found)
-                        dropdown.find("span."+genders[i].replace(/\W/g,"_")).remove();
-                }
-                // prefer not to disclose is placed at the bottom
-                // if it's already in the list, put it back at the bottom
-                if (options.allowPreferNotToDisclose) {
-                    if (dropdown.find("span.prefer_not_to_disclose").length==0) {
-                        $("<span class='prefer_not_to_disclose'>prefer not to disclose</span>").appendTo(dropdown)
-                        .click(function() {
-                            dropdown.parent().find('input').val($(this).text()).focus();
-                            findGenders(dropdown, $(this).text());
-                        });
-                    }
-                    else
-                        dropdown.find("span.prefer_not_to_disclose").detach().appendTo(dropdown);
-                }
-                if (dropdown.find("span").length == 0)
-                    dropdown.hide();
-                else
-                    dropdown.show();
-            }
+
             this.each(function(idx,element) {
                 var $this = $(element);
                 var wrapper = $("<div></div>").css('position','relative').css('display',$this.css('display')).addClass("genderField-wrapper");
                 if (options.showArrow)
                     wrapper.addClass("genderFieldSelectArrow");
                 if (options.lowercase) {
-                    for(var i = 0; i < genders.length; i++) {
-                        genders[i] = genders[i].toLowerCase();
+                    for(var i = 0; i < localGenders.length; i++) {
+                        localGenders[i] = localGenders[i].toLowerCase();
                     }
                 }
+                genderSearchMap = generateSearchMap(localGenders);
                 $this.addClass("genderField").wrap(wrapper);
 
                 var dropdown = $("<div></div>").addClass("genderField-dropdown");
                 $this.parent().append(dropdown);
                 resize($this);
-                findGenders(dropdown, $this.val());
+                var foundGenders = findGenders(localGenders, genderSearchMap, dropdown, $this.val());
+                addDropdownGenders(dropdown);
+                setDropdownGenders(dropdown, foundGenders);
                 dropdown.hide();
                 var self = $this;
 
@@ -176,18 +188,18 @@
                             event.returnValue=false;
                             event.preventDefault();
                             if (dropdown.find(".selected").length==0) {
-                                var child = dropdown.find("span:last-child").addClass("selected");
+                                var child = dropdown.find("span.shownGender").last().addClass("selected");
                                 dropdown.scrollTop(0);
                                 dropdown.scrollTop(child.position().top-child.height());
                             }
-                            else if (dropdown.find("span.selected").prev().length>0) {
-                                var child = dropdown.find("span.selected").removeClass("selected").prev().addClass("selected");
+                            else if (dropdown.find("span.selected").prev(".shownGender").length>0) {
+                                var child = dropdown.find("span.shownGender.selected").removeClass("selected").prev().addClass("selected");
                                 dropdown.scrollTop(0);
                                 dropdown.scrollTop(child.position().top-child.height());
                             }
                             else {
                                 dropdown.find("span.selected").removeClass("selected");
-                                var child = dropdown.find("span:last-child").addClass("selected");
+                                var child = dropdown.find("span.shownGender").last().addClass("selected");
                                 dropdown.scrollTop(0);
                                 dropdown.scrollTop(child.position().top-child.height());
                             }
@@ -196,17 +208,17 @@
                             event.returnValue=false;
                             event.preventDefault();
                             if (dropdown.find(".selected").length==0) {
-                                var child = dropdown.find("span:first-child").addClass("selected");
+                                var child = dropdown.find("span.shownGender").first().addClass("selected");
                                 dropdown.scrollTop(0);
                             }
-                            else if (dropdown.find("span.selected").next().length>0) {
-                                var child = dropdown.find("span.selected").removeClass("selected").next().addClass("selected");
+                            else if (dropdown.find("span.selected").next(".shownGender").length>0) {
+                                var child = dropdown.find("span.shownGender.selected").removeClass("selected").next().addClass("selected");
                                 dropdown.scrollTop(0);
                                 dropdown.scrollTop(child.position().top-child.height());
                             }
                             else {
                                 dropdown.find("span.selected").removeClass("selected");
-                                var child = dropdown.find("span:first-child").addClass("selected");
+                                var child = dropdown.find("span.shownGender").first().addClass("selected");
                                 dropdown.scrollTop(0);
                                 dropdown.scrollTop(child.position().top-child.height());
                             }
@@ -215,7 +227,8 @@
                             event.returnValue=false;
                             event.preventDefault();
                             self.val(dropdown.find("span.selected").text());
-                            findGenders(dropdown, dropdown.find("span.selected").text());
+                            var foundGenders = findGenders(localGenders, genderSearchMap, dropdown, dropdown.find("span.selected").text());
+                            setDropdownGenders(dropdown, foundGenders);
                             dropdown.find("span.selected").removeClass("selected");
                             dropdown.scrollTop(0);
                         }
@@ -228,19 +241,23 @@
                     var value = $(this).val();
                     if (value == self.data('lastValue') && value != "")
                         return;
-                    self.data('lastvalue',value);
-                    findGenders(dropdown,value);
+                    self.data('lastValue',value);
+                    var foundGenders = findGenders(localGenders, genderSearchMap, dropdown, value);
+
+                    setDropdownGenders(dropdown, foundGenders);
                 });
                 $this.change(function(event) {
                     var dropdown = $(this).parent().find('.genderField-dropdown');
                     var value = $(this).val();
-                    findGenders(dropdown,value);
+                    var foundGenders = findGenders(localGenders, genderSearchMap, dropdown, value);
+                    setDropdownGenders(dropdown, foundGenders);
                 });
                 $this.mouseup(function(event) {
                     setTimeout(function() {
                         var dropdown = self.parent().find('.genderField-dropdown');
                         var value = self.val();
-                        findGenders(dropdown,value);
+                        var foundGenders = findGenders(localGenders, genderSearchMap, dropdown, value);
+                        setDropdownGenders(dropdown, foundGenders);
                     },40);
                 });
                 $this.focus(function(event) {
@@ -248,7 +265,8 @@
                     var value = $(this).val();
                     dropdown.show();
                     resize(self);
-                    findGenders(dropdown,value);
+                    var foundGenders = findGenders(localGenders, genderSearchMap, dropdown, value);
+                    setDropdownGenders(dropdown, foundGenders);
                 });
 
                 $this.blur(function(event) {
